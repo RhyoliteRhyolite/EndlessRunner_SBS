f429a96969f1f1e77f7c7c5c27f4e42105817601
유문암 <rhyolite199@gmail.com>
2024-09-12 19:45:38 +0900

    24.09.12.목

--- Assets/Skripts/SceneryManager.cs
+++ Assets/Skripts/SceneryManager.cs
@@ -1,18 +1,86 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneryManager : MonoBehaviour
 {
-    void Start()
-    {
+    [SerializeField] Image screenImage;
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
     }
 
-    private void Update()
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(1);
+            StartCoroutine(AsyncLoad(1));
         }
+    }
+
+    public IEnumerator FadeIn()
+    {
+        screenImage.gameObject.SetActive(true);
+        Color color = screenImage.color;
+
+        color.a = 1f;
+
+        while (color.a >= 0.0f)
+        {
+            color.a -= Time.deltaTime;
+            screenImage.color = color;
+            yield return null;
+        }
+        screenImage.gameObject.SetActive(false);
+    }
+
+    public IEnumerator AsyncLoad(int index)
+    {
+        screenImage.gameObject.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+
+        asyncOperation.allowSceneActivation = false;
+
+        Color color = screenImage.color;
+        color.a = 0;
+
+        while (asyncOperation.isDone == false)
+        {
+            color.a += Time.deltaTime;
+            screenImage.color = color;
+
+            if (asyncOperation.progress >= 0.9f)
+            {
+                color.a = Mathf.Lerp(color.a, 1f, Time.deltaTime);
+                screenImage.color = color;
+
+                if (color.a >= 1.0f)
+                {
+                    asyncOperation.allowSceneActivation = true;
+
+                    yield break;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    void OnSceneLoaded(Scene sceme, LoadSceneMode loadSceneMode)
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
